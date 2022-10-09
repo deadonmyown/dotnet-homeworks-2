9bdd8d9a857285343fc905fb0bee854df1588cfb
deadonmyown <xPr1esT@yandex.ru>
2022-10-09 20:50:50 +0300

    hw4 solve

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -1,9 +1,10 @@
 ﻿module Hw4.Parser
 
 open System
+open System.Diagnostics.CodeAnalysis
 open Hw4.Calculator
 
-
+[<ExcludeFromCodeCoverage>]
 type CalcOptions = {
     arg1: float
     arg2: float
@@ -11,10 +12,26 @@ type CalcOptions = {
 }
 
 let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+    args.Length = 3
 
-let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+let parseOperation (arg: string) =
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ ->  InvalidOperationException() |> raise
     
+let tryParse (s: string) =
+    match System.Double.TryParse(s) with
+    | true, value -> value
+    | _ -> ArgumentException() |> raise
+
 let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    if(not (isArgLengthSupported args)) then
+        ArgumentException() |> raise
+    let val1 = tryParse args[0]
+    let val2 = tryParse args[2]
+    let operation = parseOperation args[1]
+    {arg1 = val1; arg2 = val2; operation = operation}
+
--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,20 @@
-﻿raise (System.NotImplementedException())
+﻿module Hw4.Program
+
+open System
+open Hw4
+
+let throwExc (exc: Exception) =
+    printfn $"{exc.Message}"
+    -1
+
+[<EntryPoint>]
+let main (args: string[]) =
+    try
+        let parsedData = Parser.parseCalcArguments args
+        printfn $"{Calculator.calculate parsedData.arg1 parsedData.operation parsedData.arg2}"
+        0
+    with
+        | :? ArgumentException as exc -> throwExc exc
+        | :? InvalidOperationException as exc -> throwExc exc
+
+        
