f76bd9e69f7ac3a12d7b92cc5e4ba00f911a53c9
deadonmyown <xPr1esT@yandex.ru>
2022-11-14 23:20:39 +0300

    solve hw10

--- Homework10/Hw10/Services/MathCalculator/MathCalculatorService.cs
+++ Homework10/Hw10/Services/MathCalculator/MathCalculatorService.cs
@@ -1,4 +1,5 @@
 using Hw10.Dto;
+using Hw10.Parser;
 
 namespace Hw10.Services.MathCalculator;
 
@@ -6,6 +7,19 @@ public class MathCalculatorService : IMathCalculatorService
 {
     public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var list = await Task.Run(() => Tokenizer.Parse(expression));
+            var postfixList = await Task.Run(() => PostfixNotation.Sort(list));
+            var expressionTree = await Task.Run(() => ExpressionTree.ConvertTokensToExpression(postfixList));
+            
+            var listExpression = await Task.Run(() => new ListExpression(expressionTree));
+            var result = await MathExpressionVisitor.VisitAsync(listExpression.Expressions);
+            return new CalculationMathExpressionResultDto(result);
+        }
+        catch(Exception ex)
+        {
+            return new CalculationMathExpressionResultDto(ex.Message);
+        }
     }
 }
