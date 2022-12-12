be010ecc1f0766632748928fcc5c1862cbd3601d
deadonmyown <xPr1esT@yandex.ru>
2022-12-12 15:04:11 +0300

    solve

--- Homework13/Hw13.Tests/MemoryTest.cs
+++ Homework13/Hw13.Tests/MemoryTest.cs
@@ -9,13 +9,12 @@ using Xunit.Abstractions;
 
 namespace Hw13.Tests;
 
-public class MemoryTest : IClassFixture<WebApplicationFactory<MemoryTest>>
-    // TODO: replace MemoryTest with the right generic argument
+public class MemoryTest : IClassFixture<WebApplicationFactory<Hw10.Program>>
 {
     private readonly HttpClient _client;
     private readonly ITestOutputHelper _output;
 
-    public MemoryTest(WebApplicationFactory<MemoryTest> factory, ITestOutputHelper output)
+    public MemoryTest(WebApplicationFactory<Hw10.Program> factory, ITestOutputHelper output)
     {
         _output = output;
         DotMemoryUnitTestOutput.SetOutputMethod(_output.WriteLine);
